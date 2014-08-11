931512856db040928a6ad20b664aa8d82ff58887
xky <mozirikan@gmail.com>
2014-08-11 23:01:30 +0900

    01 day.

---
+++ src/01 Hello World/main.d
@@ -0,0 +1,7 @@
+import std.stdio;
+
+void main()
+{
+	writeln("안녕!");
+	writeln("Hello World!");
+}
---
+++ src/03 type/main.d
@@ -0,0 +1,19 @@
+import std.stdio;
+
+void main()
+{
+	/*
+	writeln("Type               : ", int.stringof );
+	writeln("Length in bytes    : ", int.sizeof );
+	writeln("Minimum value      : ", int.min );
+	writeln("Maximum value      : ", int.max );
+	writeln("");
+	writeln(size_t.stringof);
+	*/
+	
+	int count;
+	
+	writeln("How many? : ", count );
+	count = 12;
+	writeln("How many? : ", count );
+}
